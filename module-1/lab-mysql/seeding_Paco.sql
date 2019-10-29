fda68a3274df4ce90dfff8b801c469f2e9f16eb1
Juan Francisco Hernández Hernández <jfcohh@gmail.com>
2019-10-29 13:08:00 -0600

    four archives create,seeding,update,delete

---
+++ module-1/lab-mysql/seeding_Paco.sql
@@ -0,0 +1,2 @@
+INSERT INTO invoices (invoice_numbre,date,model,inventory_id,customer_full_name,salesperson) 
+VALUES (731166526,'2019/01/30','Alfa Romeo C',0002,'Gabriel Hermsworth','Walter Melon');
---
+++ module-1/lab-mysql/update_Paco.sql
@@ -0,0 +1,10 @@
+update customers set 
+email = 'ppicasso@gmail.com' 
+where full_name='Pablo Picasso';
+update customers set 
+email = 'lincoln@us.gov' 
+where full_name='Abraham Lincoln';
+update customers set 
+email = 'hello@napoleon.me' 
+where full_name='Napoleon Bonaparte';
+
